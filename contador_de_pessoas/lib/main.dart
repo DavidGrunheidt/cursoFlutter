762a71ba5f6ce7e6aecf9e8dd2adeeab79068386
DavidGrunheidt <davidordine98@gmail.com>
2020-06-30 17:15:53 -0300

    Calculadora IMC

--- contador_de_pessoas/lib/main.dart
+++ contador_de_pessoas/lib/main.dart
@@ -23,7 +23,7 @@ class _HomeState extends State<Home> {
 
       if (_peopleCounter < 0) {
         _infoText = "Mundo invertido?!";
-      } else if (_peopleCounter > 0 && _peopleCounter <= 10) {
+      } else if (_peopleCounter >= 0 && _peopleCounter <= 10) {
         _infoText = "Pode entrar!";
       } else {
         _infoText = "Lotado";
